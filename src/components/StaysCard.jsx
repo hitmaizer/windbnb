d1a59f3e6f3dd9a4bdfbb83d5d8d6a0414f98850
hitmaizer <jose.sousalves@gmail.com>
2022-02-11 14:44:51 +0000

    conditional rendering from props

--- src/components/StaysCard.jsx
+++ src/components/StaysCard.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import { StarFill } from '@styled-icons/bootstrap/StarFill'
 import StyledCard from '../elements/StyledCard'
 
-export default function StaysCard() {
+export default function StaysCard(props) {
     return (
         <StyledCard className="card__wrapper">
-            <img src="images/test.jpg" alt="" className="card__img" />
+            <img src={props.image} alt="" className="card__img" />
             <div className="stays__details flex-row">
                 <div className="details__left flex-row">
+                    {props.superHost && 
                     <div className="host__badge">
                         <h6 className="badge__text">SUPER HOST</h6>
-                    </div>
-                    <p className="details__description">Entire apartment. 2 beds</p>
+                    </div>}
+                    
+                    <p className="details__description">{props.type} {props.beds && `${props.beds} beds`}</p>
                 </div>
                 <div className="details__right flex-row">
                     <StarFill size="16px" className="rating__icon"/>
-                    <p className="details__rating">4.40</p>
+                    <p className="details__rating">{props.rating}</p>
                 </div>
             </div>
-            <h3 className="stays__title">Stylist apartment in center of the city</h3>
+            <h3 className="stays__title">{props.title}</h3>
         </StyledCard>
     )
 }
