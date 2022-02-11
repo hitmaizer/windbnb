bc1544d2d95d14589b0bea8d2b73ef7b90be1f2c
hitmaizer <jose.sousalves@gmail.com>
2022-02-11 16:07:10 +0000

    data props to searchbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Searchbar from './Searchbar';
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
         <nav className="navbar flex-row">
             <div className="navbar__logo">
                 <img src="images/logo.svg" alt="" className="navbar__img" />
             </div>
-            <Searchbar />
+            <Searchbar data={props.data}/>
         </nav>
     )
 }
