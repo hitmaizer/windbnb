bdc9cf64cebcec30078b9928f291ba75a45bce57
hitmaizer <jose.sousalves@gmail.com>
2022-02-11 11:55:38 +0000

    added font + color variables on Theme + SCSS

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,3 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
+
 export default {
+    colors: {
+        white: "#FFFFFF",
+        primary: "#EB5757",
+        lightPrimary: "#EB5757B8 ",
+        darkGray: "#333333",
+        lighterGray: "#BDBDBD",
+        mediumGray: "#4F4F4F",
+        lightGray: "#828282",
+        offWhite: "#F2F2F2",
+
+    },
     
+    fonts: {
+        montserrat : "'Montserrat', sans-serif",
+        mulish: "'Mulish', sans-serif",
+        poppins: "'Poppins', sans-serif"
+    },
+
+    dropShadow: "0px 1px 6px rgba(0, 0, 0, 0.1)",
+
 }
