ff3bb2aa27000dd2b7ef47512b4e3b50ee8023b4
hitmaizer <jose.sousalves@gmail.com>
2022-02-11 14:44:15 +0000

    data fetch + props to hero component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import { ThemeProvider } from 'styled-components';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Theme from './theme/Theme'
+import Data from './stays.json'
 
 
 export default function App() {
+    const [data, setData] = React.useState([])
+
+    React.useEffect(() => {
+        const fetchedData = Data
+        setData(fetchedData)
+    }, [])
+
+    console.log(data)
+
     return (
         <ThemeProvider theme={Theme}>
             <div className="page__wrapper">
                 <Navbar />
-                <Hero />
+                <Hero data={data}/>
                 <footer className="footer__sign">created by <b><u>Jose Alves</u></b> - devChallenges.io </footer>
             </div>
         </ThemeProvider>
