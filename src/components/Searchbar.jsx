c327df5c0afaf23ca290010afbcd4ef07dde68c1
hitmaizer <jose.sousalves@gmail.com>
2022-02-12 12:14:16 +0000

    passed down props to search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -7,12 +7,12 @@ import { Close } from '@styled-icons/evaicons-solid/Close'
 import Button from '@mui/material/Button';
 
 export default function Searchbar(props) {
-    const [filteredData, setFilteredData] = React.useState([])
-    const [wordEntered, setWordEntered] = React.useState("")
+    /* const [filteredData, setFilteredData] = React.useState([])
+    const [wordEntered, setWordEntered] = React.useState("") */
     const [state, setState] = React.useState(false)
-    const dataSearch = props.data
+    // const dataSearch = props.data
 
-    const handleFilter = (event) => {
+    /* const handleFilter = (event) => {
         const searchWord = event.target.value
         setWordEntered(searchWord)
         const newFilter = dataSearch.filter((value) => {
@@ -23,21 +23,19 @@ export default function Searchbar(props) {
         } else {
             setFilteredData(newFilter)
         }
-    }
+    } */
 
-    const clearInput = () => {
+    /* const clearInput = () => {
         setFilteredData([])
         setWordEntered("")
-    }
+    } */
 
     //your first function
     function toggleDrawer(open) {
-
         //your secound function
             return function (event) {
                 return setState(open)
             }
-      
         }
 
     return (
@@ -45,7 +43,7 @@ export default function Searchbar(props) {
             <div className="navbar__search flex-row">
                     <div className="search__location">
                         <Button onClick={toggleDrawer(true)}> 
-                            <input type="text" className="search__input" placeholder='Search Location' value={wordEntered} onChange={handleFilter} />
+                            <input type="text" className="search__input" placeholder='Search Location' value={props.wordEntered} onChange={(e) => props.handleFilter(props.event, e)} />
                         </Button>
                         <Drawer
                             anchor={'top'}
@@ -56,21 +54,21 @@ export default function Searchbar(props) {
                             <div className="searchbar__wrapper searchbar__drawer">
                                 <div className="navbar__search flex-row drawer">
                                     <div className="search__location">
-                                        <input type="text" className="search__input input--drawer" placeholder='Search Location' value={wordEntered} onChange={handleFilter} />
+                                        <input type="text" className="search__input input--drawer" placeholder='Search Location' value={props.wordEntered} onChange={(e) => props.handleFilter(props.event, e)} />
                                     </div>
 
                                     <div className="search__guests">
                                         <input type="number" className="search__input input--drawer" placeholder="Add guests" />
                                     </div>
                                     <div className="search__icon">
-                                        {filteredData.length === 0 ? <Search size="24px" /> : <Close size="24px" id="clearBtn" onClick={clearInput} />}
+                                        {props.filteredData.length === 0 ? <Search size="24px" /> : <Close size="24px" id="clearBtn" onClick={props.clearInput} />}
                                     </div>
                                 </div>
                             </div>
                             <div className="results flex-row">
-                                {filteredData.length !== 0 && 
+                                {props.filteredData.length !== 0 && 
                                     <div className="data__result">
-                                    {filteredData.map((value, key) => {
+                                    {props.filteredData.map((value, key) => {
                                         return (
                                             <div className="result flex-row" key={nanoid()}>
                                                 <LocationPin size="24px" className="result__icon" />
@@ -90,13 +88,13 @@ export default function Searchbar(props) {
                     </Button>
                     </div>
                     <div className="search__icon">
-                        {filteredData.length === 0 ? <Search size="24px" /> : <Close size="24px" id="clearBtn" onClick={clearInput} />}
+                        {props.filteredData.length === 0 ? <Search size="24px" /> : <Close size="24px" id="clearBtn" onClick={props.clearInput} />}
                         
                     </div>
             </div>
-                    {filteredData.length !== 0 && 
+                    {props.filteredData.length !== 0 && 
                     <div className="data__result">
-                        {filteredData.map((value, key) => {
+                        {props.filteredData.map((value, key) => {
                             return (
                                 <div className="result flex-row" key={nanoid()}>
                                     <LocationPin size="24px" className="result__icon" />
